9929035a02e0daec935f409bd5fc8ff5f6651e3f
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2026-07-05 23:05:03 +0530

    Add Main.java to implement basic flight welcome messages

---
+++ Flight 002 - Basics/Main.java
@@ -0,0 +1,7 @@
+package FirstProject.src;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java Airlines");
+        System.out.println("Flight 002 - Basics");
+    }
+}
